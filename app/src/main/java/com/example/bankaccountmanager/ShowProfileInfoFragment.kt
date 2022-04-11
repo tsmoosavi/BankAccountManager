26ea7f6e84faa087f2cee88d374fcd61e7fb951f
ts moosavi <tt.musavi@gmail.com>
2022-04-12 02:28:36 +0430

    ShowProfileInfoFragment.kt coding completed

--- app/src/main/java/com/example/bankaccountmanager/ShowProfileInfoFragment.kt
+++ app/src/main/java/com/example/bankaccountmanager/ShowProfileInfoFragment.kt
@@ -1,10 +1,14 @@
 package com.example.bankaccountmanager
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import com.example.bankaccountmanager.databinding.FragmentShowProfileInfoBinding
 
 
@@ -22,6 +26,25 @@ class ShowProfileInfoFragment : Fragment() {
         binding = FragmentShowProfileInfoBinding.inflate(inflater,container,false)
         // Inflate the layout for this fragment
         return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        showSavedInfo()
+        binding.edit.setOnClickListener{
+            findNavController().navigate(R.id.action_showProfileInfoFragment_to_profileFragment)
+        }
+
+    }
+
+    private fun showSavedInfo() {
+        var saveInfo: SharedPreferences =
+            requireActivity().getSharedPreferences("personalInformation", Context.MODE_PRIVATE)
+        binding.fullNameTv.setText(saveInfo.getString("savedName", null))
+        binding.fatherNameTv.setText(saveInfo.getString("savedFather'sName", null))
+        binding.phone.setText(saveInfo.getString("savedPhone", null))
+        binding.postalCodeTv.setText(saveInfo.getString("savedPostalCode", null))
+        binding.numberOfBankAccountsTv.setText(saveInfo.getString("savedNumberOfBankAccount", null))
+
     }
 
 }
