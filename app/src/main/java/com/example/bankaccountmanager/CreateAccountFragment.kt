d8ac28633778091cd2fede06b7968fd71952856d
ts moosavi <tt.musavi@gmail.com>
2022-04-12 16:46:46 +0430

    spinner added

--- app/src/main/java/com/example/bankaccountmanager/CreateAccountFragment.kt
+++ app/src/main/java/com/example/bankaccountmanager/CreateAccountFragment.kt
@@ -5,11 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import com.example.bankaccountmanager.databinding.FragmentCreateAccountBinding
 
 
-class CreateAccountFragment : Fragment() {
-
-
+class CreateAccountFragment : Fragment(), AdapterView.OnItemSelectedListener  {
+        lateinit var binding: FragmentCreateAccountBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -19,8 +21,31 @@ class CreateAccountFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentCreateAccountBinding.inflate(inflater,container,false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_account, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        var spinner = binding.spinner
+
+        ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.accountType,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+
+        }
+
+    }
+
+    override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+        var accountType :String= p0?.getItemAtPosition(p2).toString()
+    }
+
+    override fun onNothingSelected(p0: AdapterView<*>?) {
+    }
 }
