c22a8e878840c3d577d2d8c43b231c246a7459e2
ts moosavi <tt.musavi@gmail.com>
2022-04-13 22:00:19 +0430

    ShowAccountVM.kt added ro showAccount fragment

---
+++ app/src/main/java/com/example/bankaccountmanager/ShowAccountVM.kt
@@ -0,0 +1,7 @@
+package com.example.bankaccountmanager
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class ShowAccountVM (app: Application): AndroidViewModel(app){
+}
